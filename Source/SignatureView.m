2dd7cf0b522f7e3c6e89a6949c05e53dcb142637
Michal Konturek <michal.konturek@gmail.com>
2014-05-05 03:03:01 +0100

    .

---
+++ Source/SignatureView.m
@@ -0,0 +1,31 @@
+//
+//  SignatureView.m
+//  SignatureView
+//
+//  Created by Michal Konturek on 05/05/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import "SignatureView.h"
+
+@implementation SignatureView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
