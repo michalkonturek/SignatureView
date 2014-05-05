918f8dcb1c26e17202ba874057309bee3905bfcc
Michal Konturek <michal.konturek@gmail.com>
2014-05-05 02:23:37 +0100

    Addded project.

---
+++ SignatureView/SignatureView/Example/ExampleViewVC.h
@@ -0,0 +1,13 @@
+//
+//  ExampleViewVC.h
+//  SignatureView
+//
+//  Created by Michal Konturek on 05/05/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExampleViewVC : UIViewController
+
+@end
---
+++ SignatureView/SignatureView/Example/ExampleViewVC.m
@@ -0,0 +1,38 @@
+//
+//  ExampleViewVC.m
+//  SignatureView
+//
+//  Created by Michal Konturek on 05/05/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import "ExampleViewVC.h"
+
+@interface ExampleViewVC ()
+
+@end
+
+@implementation ExampleViewVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
